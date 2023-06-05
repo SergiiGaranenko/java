ca60eba7b644c4b86fa4dc153b2d42460f06bfc2
SergiiGaranenko <s.garanenko@andersenlab.com>
2023-06-05 12:31:02 +0300

    feign retry

--- feign/src/main/java/com/demo/feign/config/FeignConfig.java
+++ feign/src/main/java/com/demo/feign/config/FeignConfig.java
@@ -3,8 +3,10 @@ package com.demo.feign.config;
 import com.demo.feign.feign.DownstreamServiceApi;
 import feign.Feign;
 import feign.Logger.Level;
+import feign.Retryer;
 import feign.jackson.JacksonDecoder;
 import feign.slf4j.Slf4jLogger;
+import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
@@ -15,6 +17,9 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FeignConfig {
+
+  private final int RETRY_INTERVAL = 2000;
+  private final int MAX_ATTEMPTS = 3;
 
   public String baseUrl = "http://localhost:8088";
   @Autowired
@@ -32,6 +37,8 @@ public class FeignConfig {
         .contract(new SpringMvcContract())
         .encoder(new PageableQueryEncoder(new SpringEncoder(this.messageConverters)))
         .decoder(new JacksonDecoder())
+        .retryer(new Retryer.Default(RETRY_INTERVAL, TimeUnit.SECONDS.toMillis(RETRY_INTERVAL),
+            MAX_ATTEMPTS))
         .target(clazz, baseUrl);
   }
 }
